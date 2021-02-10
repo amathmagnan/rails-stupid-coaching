07b97024716a52d98e49165220d3435d6cc2be31
Amath <amathmagnan@gmail.com>
2021-02-10 20:39:12 +0100

    question stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,16 @@ def ask
 end
 
 
+
+  def answer
+  if params[:question] == 'I am going to work'
+  puts @answer = 'Great!'
+elsif params[:question] == '?'
+  puts @answer = 'Silly question, get dressed and go to work!.'
+else
+  puts @answer = "I don't care, get dressed and go to work!"
+  end
+
+
+end
 end
